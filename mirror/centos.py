c41cc592a42bbfe4a401fba102fbe370ad31899b
Ken Tossell <ken@tossell.net>
2011-12-25 15:17:42 -0500

    turned permissions on for centos (which masked its last release)

--- mirror/centos.py
+++ mirror/centos.py
@@ -3,4 +3,5 @@ from . import RsyncMirrorRunner
 class CentOSMirrorRunner(RsyncMirrorRunner):
   source = 'rsync://us-msync.centos.org/CentOS/'
   # rsync_filter_list = ['- /3*', '- /2*']
+  rsync_preserve_perm = True
 
