cba53edc2718423c75024ec8db9f0e685a21cf3b
Ken Tossell <ken@tossell.net>
2012-07-11 14:25:51 -0400

    switched from rit to gatech

--- mirror/archlinux.py
+++ mirror/archlinux.py
@@ -1,7 +1,9 @@
 from . import RsyncMirrorRunner
 
 class ArchLinuxMirrorRunner(RsyncMirrorRunner):
-  source = 'rsync://mirror.rit.edu/archlinux/'
+  #source = 'rsync://mirror.rit.edu/archlinux/'
+  # rit is often over capacity (too many connections) or drops connections (5/11/2012)
+  source = 'rsync://rsync.gtlib.gatech.edu/archlinux/'
   #rsync_filter_list = ['- iso/2007.*', '- iso/2008.*', '- iso/2009.*']
   rsync_delete_delay = True
   rsync_delay_updates = True
