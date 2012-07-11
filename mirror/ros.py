889640b82c3c96c0fdc2064d8b2cba6254b495b5
Ken Tossell <ken@tossell.net>
2012-07-11 14:25:15 -0400

    added 12.04 for ros

--- mirror/ros.py
+++ mirror/ros.py
@@ -2,7 +2,7 @@ from . import APTMirrorRunner
 
 class ROSMirrorRunner(APTMirrorRunner):
   source = 'http://packages.ros.org/ros/ubuntu'
-  apt_releases = ['jaunty', 'karmic', 'lucid', 'maverick', 'natty', 'oneiric']
+  apt_releases = ['jaunty', 'karmic', 'lucid', 'maverick', 'natty', 'oneiric', 'precise']
   apt_architectures = ['i386', 'amd64', 'src']
   apt_parts = ['main']
 
