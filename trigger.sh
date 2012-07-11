0e5e199075b75901ff6d3919edb9ca33612af590
Ken Tossell <ken@tossell.net>
2012-07-11 14:25:31 -0400

    switched to ssh triggering

--- trigger.sh
+++ trigger.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-curl http://mirror:d2ccd17f9974829623f6841641c3795c@localhost:8080/jenkins/job/$1/build?cause=SSH+Push
+#curl http://mirror:d2ccd17f9974829623f6841641c3795c@localhost:8080/jenkins/job/$1/build?cause=SSH+Push
+ssh mirror@localhost -p 36518 build $1
 
