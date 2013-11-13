9eb5b6f10b828deeef1b78dac2b7dfeb6ff4ff7f
Ken Tossell <ken@tossell.net>
2013-11-12 19:40:49 -0500

    ros: use ubuntu release list. added ARM architectures.

--- mirror/ros.py
+++ mirror/ros.py
@@ -2,7 +2,7 @@ from . import APTMirrorRunner
 
 class ROSMirrorRunner(APTMirrorRunner):
   source = 'http://packages.ros.org/ros/ubuntu'
-  apt_releases = ['jaunty', 'karmic', 'lucid', 'maverick', 'natty', 'oneiric', 'precise']
-  apt_architectures = ['i386', 'amd64', 'src']
+  apt_releases = list(set(APTMirrorRunner.ALL_UBUNTU_RELEASES[7:] + ['wheezy']) - set(['intrepid']))
+  apt_architectures = ['i386', 'amd64', 'armel', 'armhf']
   apt_parts = ['main']
 
