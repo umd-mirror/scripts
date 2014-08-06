ef11372b19fd9e487d0da5882385a7be19b62ee4
Ken Tossell <ken@tossell.net>
2014-08-05 20:44:33 -0400

    added gnome

---
+++ mirror/gnome.py
@@ -0,0 +1,11 @@
+from . import RsyncMirrorRunner, EXTRAS
+import os
+
+class GNOMEMirrorRunner(RsyncMirrorRunner):
+  source = 'rsync://master.gnome.org/gnomeftp/'
+  rsync_delete_delay = True
+  rsync_preserve_perm = True
+
+  def pre_update(self, verbose, dry_run):
+    os.environ['RSYNC_PASSWORD'] = open(os.path.join(EXTRAS, 'gnome-password'), "r").read().strip()
+
