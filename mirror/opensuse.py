e0fa4006a0a7af70b45598730f5a9cf7bba3bdd4
Ken Tossell <ken@tossell.net>
2014-08-05 20:17:44 -0400

    switched to stage.opensuse.org

--- mirror/opensuse.py
+++ mirror/opensuse.py
@@ -1,6 +1,6 @@
 from . import RsyncMirrorRunner
 
 class OpenSUSEMirrorRunner(RsyncMirrorRunner):
-  source = 'rsync://rsync.opensuse.org/opensuse-hotstuff-160gb/'
+  source = 'rsync://stage.opensuse.org/opensuse-hotstuff-160gb/'
   rsync_preserve_perm = True
 
