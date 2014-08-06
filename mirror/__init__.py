102f34a6f44df645a06f0c63e51cff1536cd32aa
Ken Tossell <ken@tossell.net>
2014-08-05 20:16:40 -0400

    updated ubuntu releases for 14.04

--- mirror/__init__.py
+++ mirror/__init__.py
@@ -174,8 +174,8 @@ class RsyncMirrorRunner(MirrorRunner):
     os.unlink(combined_filter_path)
 
 class APTMirrorRunner(MirrorRunner):
-  ALL_UBUNTU_RELEASES = ['warty', 'hoary', 'breezy', 'dapper', 'edgy', 'feisty', 'gutsy', 'hardy', 'intrepid', 'jaunty', 'karmic', 'lucid', 'maverick', 'natty', 'oneiric', 'precise', 'quantal', 'raring', 'saucy']
-  LTS_UBUNTU_RELEASES = ['dapper', 'hardy', 'lucid', 'precise']
+  ALL_UBUNTU_RELEASES = ['warty', 'hoary', 'breezy', 'dapper', 'edgy', 'feisty', 'gutsy', 'hardy', 'intrepid', 'jaunty', 'karmic', 'lucid', 'maverick', 'natty', 'oneiric', 'precise', 'quantal', 'raring', 'saucy', 'trusty']
+  LTS_UBUNTU_RELEASES = ['dapper', 'hardy', 'lucid', 'precise', 'trusty']
 
   apt_releases = []
   apt_architectures = []
