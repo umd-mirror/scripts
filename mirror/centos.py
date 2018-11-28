f8de5565f74c813e822dd5228f863b9a5d7c4b79
Mirror <mirror@mirror.umd.edu>
2018-11-28 17:58:47 +0000

    centos fix hardlinks

--- mirror/centos.py
+++ mirror/centos.py
@@ -4,4 +4,4 @@ class CentOSMirrorRunner(RsyncMirrorRunner):
   source = 'rsync://us-msync.centos.org/CentOS/'
   # rsync_filter_list = ['- /3*', '- /2*']
   rsync_preserve_perm = True
-
+  rsync_preserve_hardlinks = True
