c2ec2233b5e463d5aad79193291713e7e3e127ea
Mirror <mirror@mirror.umd.edu>
2020-01-26 18:06:21 +0000

    fix typo in gentoo-portage rsync url

--- mirror/gentoo-portage.py
+++ mirror/gentoo-portage.py
@@ -2,4 +2,4 @@ from . import RsyncMirrorRunner
 
 class GentooPortageMirrorRunner(RsyncMirrorRunner):
   # source = 'rsync://ftp.ussg.iu.edu/gentoo-portage/'
-  source = 'rsyc://mirrors.rit.edu/gentoo-rsync/'
+  source = 'rsync://mirrors.rit.edu/gentoo-rsync/'
