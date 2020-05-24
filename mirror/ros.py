e82be5360c98955054190e223241d362e82a4ea9
Mirror <mirror@mirror.umd.edu>
2020-05-24 16:58:55 +0000

    Update ROS distro list
    See https://discourse.ros.org/t/updated-mirror-list-available/14250

--- mirror/ros.py
+++ mirror/ros.py
@@ -3,7 +3,7 @@ from . import APTMirrorRunner
 class ROSMirrorRunner(APTMirrorRunner):
   source = 'http://packages.ros.org/ros/ubuntu'
   # get updates from https://github.com/ros-infrastructure/mirror/blob/master/mirror/files/mirror.list
-  apt_releases = list(set(['jessie', 'stretch', 'precise', 'trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic']))
+  apt_releases = list(set(['jessie', 'stretch', 'buster', 'trusty', 'xenial', 'bionic', 'focal']))
   apt_architectures = ['i386', 'amd64', 'arm64', 'armhf']
   apt_parts = ['main']
 
