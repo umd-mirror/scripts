e63b4a3a9ea687869ed8e38d96fde734daa69e76
Mirror <mirror@mirror.umd.edu>
2021-07-22 18:16:48 +0000

    use official eclipse upstream

--- mirror/eclipse.py
+++ mirror/eclipse.py
@@ -2,7 +2,8 @@ from . import RsyncMirrorRunner
 
 class EclipseMirrorRunner(RsyncMirrorRunner):
   #source = 'rsync://rsync.osuosl.org/eclipse'
-  source = 'rsync://rsync.gtlib.gatech.edu/eclipse'
+  #source = 'rsync://rsync.gtlib.gatech.edu/eclipse'
+  source = 'rsync://download.eclipse.org/eclipseMirror'
 
   rsync_delete_delay = True
   rsync_delay_updates = True
