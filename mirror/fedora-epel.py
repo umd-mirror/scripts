532065e365e0bdde4a2f43d78f2296419a8c3c65
Mirror <mirror@mirror.umd.edu>
2022-03-04 17:21:35 +0000

    better upstream for fedora epel

--- mirror/fedora-epel.py
+++ mirror/fedora-epel.py
@@ -6,11 +6,11 @@ from . import RsyncMirrorRunner, EXTRAS
 class FedoraEPELMirrorRunner(RsyncMirrorRunner):
   base_subdir = 'fedora/epel'
   # source = 'rsync://download.wpi.edu:874/fedora-epel/'
-  # source = 'rsync://archive.linux.duke.edu/fedora-epel/'
+  source = 'rsync://archive.linux.duke.edu/fedora-epel/'
   # source = 'rsync://fedora-archives.ibiblio.org/fedora-epel/'
   # source = 'rsync://ftp.heanet.ie/fedora-epel/'
   # source = 'rsync://mirror.rit.edu/epel/'
-  source = 'rsync://mirror.gtlib.gatech.edu/fedora-epel/'
+  # source = 'rsync://rsync.gtlib.gatech.edu/fedora-epel/'
   rsync_filter_list = ['- debug', '- SRPMS', '- /3', '- /4', '- /5', '- /4AS', '- /4ES', '- /4WS', '- /5Client', '- /5Server']
   #rsync_filter_list = ['- debug', '- /testing', '- SRPMS', '- /3', '- /4', '- /5']
   rsync_preserve_hardlinks = True
