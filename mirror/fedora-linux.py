84e5383c543ae345636fa28c65849bc1f0cc6a49
Mirror <mirror@mirror.umd.edu>
2023-04-24 15:41:56 +0000

    switch to duke for fedora

--- mirror/fedora-linux.py
+++ mirror/fedora-linux.py
@@ -7,10 +7,9 @@ class FedoraLinuxMirrorRunner(RsyncMirrorRunner):
   base_subdir = 'fedora/linux'
   #source = 'rsync://fedora-archives.ibiblio.org/fedora-enchilada/linux/'
   # source = 'rsync://archive.linux.duke.edu:874/fedora/linux/'
-  source = 'rsync://mirrors.rit.edu/fedora-enchilada/linux/'
-  # source = 'rsync://archive.linux.duke.edu/fedora-enchilada/linux/'
+  # source = 'rsync://mirrors.rit.edu/fedora-enchilada/linux/'
+  source = 'rsync://archive.linux.duke.edu/fedora-enchilada/linux/'
   # source = 'rsync://ftp.heanet.ie/fedora-enchilada/linux/'
-  #source = 'rsync://mirror.rit.edu/fedora-enchilada/fedora/linux/'
 
   rsync_filter_from = os.path.join(EXTRAS, 'fedora-linux.exclude')
   rsync_preserve_hardlinks = True
