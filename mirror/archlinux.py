6b59e7fb78025c820f3201cbe6ee713809ff9331
Yehuda Katz <yakatz@whiteoak.umd.edu>
2024-07-09 18:29:48 +0000

    Update arch mirror source

--- mirror/archlinux.py
+++ mirror/archlinux.py
@@ -5,7 +5,9 @@ class ArchLinuxMirrorRunner(RsyncMirrorRunner):
   # rit is often over capacity (too many connections) or drops connections (5/11/2012)
   #source = 'rsync://rsync.gtlib.gatech.edu/archlinux/'
   # gatech times out frequently and has a poor mirror score (9/28/2013)
-  source = 'rsync://mirror.us.leaseweb.net/archlinux/'
+  # Leaseweb no longer worrks either (YAK - 2024-07-09)
+  #source = 'rsync://mirror.us.leaseweb.net/archlinux/'
+  source = 'rsync://mirrors.lug.mtu.edu/archlinux/'
   #source = 'rsync://archlinux.polymorf.fr/archlinux/'
   #rsync_filter_list = ['- iso/2007.*', '- iso/2008.*', '- iso/2009.*']
   rsync_delete_delay = True
