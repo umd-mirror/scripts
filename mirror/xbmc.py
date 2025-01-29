6d7e59872d820af6a1c03378b11089f88f10e7a7
Mirror <mirror@mirror.umd.edu>
2025-01-29 16:37:04 +0000

    direct xbmc sync

--- mirror/xbmc.py
+++ mirror/xbmc.py
@@ -1,5 +1,4 @@
 from . import RsyncMirrorRunner
 
 class XBMCMirrorRunner(RsyncMirrorRunner):
-  # source = 'rsync://ftp.heanet.ie/mirrors/xbmc/'
-  source = 'rsync://rsync.gtlib.gatech.edu/xbmc/'
+  source = 'rsync://rsync.kodi.tv/main/'
